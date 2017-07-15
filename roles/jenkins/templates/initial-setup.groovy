729ceba0e885e90eaf68def73e3998c78c76df30
Bimlendu Mishra <justbimlendu@gmail.com>
2017-07-15 23:26:13 +0530

    more initial setup

---
+++ roles/jenkins/templates/initial-setup.groovy
@@ -0,0 +1,63 @@
+#!groovy
+import jenkins.*
+import jenkins.model.*
+import hudson.*
+import hudson.model.*
+import hudson.security.*
+import jenkins.security.s2m.AdminWhitelistRule
+import java.util.logging.Logger
+
+def logger = Logger.getLogger("")
+def installed = false
+def initialized = false
+def pluginParameter = "{{ jenkins_plugins }}"
+def plugins = pluginParameter.split()
+logger.info(plugins)
+def jenkins = Jenkins.getInstance()
+def pm = jenkins.getPluginManager()
+def uc = jenkins.getUpdateCenter()
+
+logger.info('Creating initial user {{ jenkins_admin_username }}.')
+
+jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
+jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
+
+def user = jenkins.getSecurityRealm().createAccount({{ jenkins_admin_username }}, {{ jenkins_admin_password }})
+user.save()
+
+jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, {{ jenkins_admin_username }})
+jenkins.save()
+
+logger.info('Enabling slave-to-master access control.')
+// https://wiki.jenkins.io/display/JENKINS/Slave+To+Master+Access+Control
+Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class)
+.setMasterKillSwitch(false)
+
+logger.info('Installing plugins.')
+plugins.each {
+  logger.info('Checking ' + it)
+  if (!pm.getPlugin(it)) {
+    logger.info('Looking UpdateCenter for ' + it)
+    if (!initialized) {
+      uc.updateAllSites()
+      initialized = true
+    }
+    def plugin = uc.getPlugin(it)
+    if (plugin) {
+      logger.info('Installing ' + it)
+    	def installFuture = plugin.deploy()
+      while(!installFuture.isDone()) {
+        logger.info('Waiting for plugin install: ' + it)
+        sleep(3000)
+      }
+      installed = true
+    }
+  }
+}
+if (installed) {
+  logger.info('Plugins installed, initializing a restart!')
+  jenkins.save()
+  jenkins.restart()
+}
+
+logger.info('Initial setup done.')
