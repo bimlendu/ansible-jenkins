fd009c674b5a5db149fede069feffbe623bf8e8f
Bimlendu Mishra <justbimlendu@gmail.com>
2017-07-15 23:28:02 +0530

    install plugins first

--- roles/jenkins/templates/initial-setup.groovy
+++ roles/jenkins/templates/initial-setup.groovy
@@ -17,6 +17,33 @@ def jenkins = Jenkins.getInstance()
 def pm = jenkins.getPluginManager()
 def uc = jenkins.getUpdateCenter()
 
+logger.info('Installing plugins.')
+plugins.each {
+  logger.info('Checking ' + it)
+  if (!pm.getPlugin(it)) {
+    logger.info('Looking UpdateCenter for ' + it)
+    if (!initialized) {
+      uc.updateAllSites()
+      initialized = true
+    }
+    def plugin = uc.getPlugin(it)
+    if (plugin) {
+      logger.info('Installing ' + it)
+      def installFuture = plugin.deploy()
+      while(!installFuture.isDone()) {
+        logger.info('Waiting for plugin install: ' + it)
+        sleep(3000)
+      }
+      installed = true
+    }
+  }
+}
+if (installed) {
+  logger.info('Plugins installed, initializing a restart!')
+  jenkins.save()
+  jenkins.restart()
+}
+
 logger.info('Creating initial user {{ jenkins_admin_username }}.')
 
 jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
@@ -32,32 +59,5 @@ logger.info('Enabling slave-to-master access control.')
 // https://wiki.jenkins.io/display/JENKINS/Slave+To+Master+Access+Control
 Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class)
 .setMasterKillSwitch(false)
-
-logger.info('Installing plugins.')
-plugins.each {
-  logger.info('Checking ' + it)
-  if (!pm.getPlugin(it)) {
-    logger.info('Looking UpdateCenter for ' + it)
-    if (!initialized) {
-      uc.updateAllSites()
-      initialized = true
-    }
-    def plugin = uc.getPlugin(it)
-    if (plugin) {
-      logger.info('Installing ' + it)
-    	def installFuture = plugin.deploy()
-      while(!installFuture.isDone()) {
-        logger.info('Waiting for plugin install: ' + it)
-        sleep(3000)
-      }
-      installed = true
-    }
-  }
-}
-if (installed) {
-  logger.info('Plugins installed, initializing a restart!')
-  jenkins.save()
-  jenkins.restart()
-}
 
 logger.info('Initial setup done.')
