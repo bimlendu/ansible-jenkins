85ef7aadc7bc1ddda1d2bf62a583ca2284748e2c
Bimlendu Mishra <justbimlendu@gmail.com>
2017-07-22 03:15:02 +0530

    pre-approve certain signatures

--- roles/jenkins/templates/initial-setup.groovy
+++ roles/jenkins/templates/initial-setup.groovy
@@ -59,4 +59,23 @@ logger.info('Enabling slave-to-master access control.')
 Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class)
 .setMasterKillSwitch(false)
 
+logger.info('Pre-approving scripts as required.')
+import org.jenkinsci.plugins.scriptsecurity.scripts.*
+ScriptApproval sa = ScriptApproval.get();
+
+signatures = [
+  "staticMethod org.codehaus.groovy.runtime.DefaultGroovyMethods getText java.net.URL",
+  "method groovy.json.JsonSlurperClassic parseText java.lang.String", 
+  "method java.lang.AutoCloseable close",
+  "new groovy.json.JsonSlurperClassic",
+  "staticMethod org.codehaus.groovy.runtime.DefaultGroovyMethods getAt java.lang.Object java.lang.String"
+]
+
+signatures.each {signature ->
+  ScriptApproval.PendingSignature s = new ScriptApproval.PendingSignature(signature, false, ApprovalContext.create())
+  sa.getPendingSignatures().add(s)
+  sa.approveSignature(s.signature)
+  logger.info('Approved signature: ' + signature)
+}
+
 logger.info('Initial setup done.')
