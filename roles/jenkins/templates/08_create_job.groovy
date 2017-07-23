f37b1f8e23bf2007e6af030187784540c540b090
Bimlendu Mishra <justbimlendu@gmail.com>
2017-07-23 15:36:23 +0530

    create gitSCM job properly

--- roles/jenkins/templates/08_create_job.groovy
+++ roles/jenkins/templates/08_create_job.groovy
@@ -10,9 +10,9 @@ import org.jenkinsci.plugins.workflow.job.WorkflowJob
 import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition
 import hudson.plugins.git.GitSCM
 import java.util.logging.Logger
+import hudson.plugins.git.*;
 
 def logger = Logger.getLogger("")
-
 def jenkins = Jenkins.getInstance()
 
 def caddy_pipeline_job = "{{ caddy_pipeline_job }}"
@@ -29,9 +29,20 @@ Jenkins.instance.getAllItems().each { j ->
   	}
 }
 
+def userRemoteConfig = new UserRemoteConfig(caddy_pipeline_repo, null, null, null)
+
+def scm = new GitSCM(
+    Collections.singletonList(userRemoteConfig),
+    Collections.singletonList(new BranchSpec("master")),
+    false,
+    Collections.<SubmoduleConfig>emptyList(),
+    null,
+    null,
+    null)
+
 if (!caddy_pipeline_exists) {
   	WorkflowJob job = Jenkins.instance.createProject(WorkflowJob, caddy_pipeline_job)
-  	def definition = new CpsScmFlowDefinition(new GitSCM(caddy_pipeline_repo),caddy_pipeline_file)
+  	def definition = new CpsScmFlowDefinition(scm,caddy_pipeline_file)
   	definition.lightweight = true
   	job.definition = definition
 }
