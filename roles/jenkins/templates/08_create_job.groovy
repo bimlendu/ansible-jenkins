3948b0bd88b25511514f9b7336ebc5d7be72b8f9
Bimlendu Mishra <justbimlendu@gmail.com>
2017-07-23 06:05:22 +0530

    scripts order

---
+++ roles/jenkins/templates/08_create_job.groovy
@@ -0,0 +1,39 @@
+#!groovy
+import jenkins.*
+import jenkins.model.*
+import hudson.*
+import hudson.model.*
+import hudson.security.*
+import hudson.tools.*
+import jenkins.model.Jenkins
+import org.jenkinsci.plugins.workflow.job.WorkflowJob
+import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition
+import hudson.plugins.git.GitSCM
+import java.util.logging.Logger
+
+def logger = Logger.getLogger("")
+
+def jenkins = Jenkins.getInstance()
+
+def caddy_pipeline_job = "{{ caddy_pipeline_job }}"
+def caddy_pipeline_repo = "{{ caddy_pipeline_repo }}"
+def caddy_pipeline_file = "{{ caddy_pipeline_file }}"
+
+logger.info('Creating pipeline job: ' + caddy_pipeline_job)
+
+def caddy_pipeline_exists = false
+Jenkins.instance.getAllItems().each { j ->
+	if (j.fullName == caddy_pipeline_job) {
+    	caddy_pipeline_exists = true
+    	logger.info('Found existing job: ' + caddy_pipeline_job)
+  	}
+}
+
+if (!caddy_pipeline_exists) {
+  	WorkflowJob job = Jenkins.instance.createProject(WorkflowJob, caddy_pipeline_job)
+  	def definition = new CpsScmFlowDefinition(new GitSCM(caddy_pipeline_repo),caddy_pipeline_file)
+  	definition.lightweight = true
+  	job.definition = definition
+}
+
+jenkins.save()
